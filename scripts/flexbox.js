e54abb701a24319b6c60c688572b457b091ab14f
Greg Bodnar <gergoe.bodnar@gmail.com>
2023-04-18 03:36:22 +0200

    addItem func

--- scripts/flexbox.js
+++ scripts/flexbox.js
@@ -1,6 +1,28 @@
 function changeProperty() {
     var flexDir = document.getElementById('flex-direction');
-    var mainWindow = document.getElementById('main-window');
+    var mainWindow = document.getElementById('flex-main-window');
     mainWindow.style.flexDirection = flexDir.value;
 }
 
+
+
+
+
+
+
+function addItem(num) {
+    let arr = document.getElementsByClassName('avitem');
+    let arr2 = document.getElementsByClassName('item');
+    for (let i = 0; i < arr.length; i++) {
+        let thisId = arr[i].getAttribute('id')
+        if (thisId.slice(-2) === num) {
+            if (arr[i].style.display === 'none') {
+                arr[i].style.display = 'block';
+                arr2[i].style.display = 'flex';
+            } else {
+                arr[i].style.display = 'none';
+                arr2[i].style.display = 'none';
+            }
+        }
+    }
+}
